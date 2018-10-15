d998a866c1da334b3133127efaa43ae7679e35f0
Helgo24 <37613342+Helgo24@users.noreply.github.com>
2018-10-15 09:53:59 +0200

    Erster Test

---
+++ Test_GitHub/Test_GitHub/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_GitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Oiso jetz müsste es gehen, probiert es mal. :)");
+            Console.ReadLine();
+        }
+    }
+}
